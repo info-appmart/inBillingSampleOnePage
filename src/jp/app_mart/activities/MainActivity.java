920c6c30ef83eaae34356fcc86d6dbb30ed40df9
info-appmart <facebook@app-mart.jp>
2013-11-06 11:12:38 +0900

    first commit

---
+++ src/jp/app_mart/activities/MainActivity.java
@@ -0,0 +1,348 @@
+package jp.app_mart.activities;
+
+import java.security.Key;
+import java.security.KeyFactory;
+import java.security.spec.KeySpec;
+import java.security.spec.X509EncodedKeySpec;
+
+import javax.crypto.Cipher;
+
+import jp.app_mart.service.AppmartInBillingInterface;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.app.PendingIntent.CanceledException;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * appMart(c) の内部課金システムサンプルコードです。 
+ * 一つのクラスで　service, receiver, activityなどをまとめました。
+ */
+public class MainActivity extends Activity {
+
+	/* 対象アプリのサービス情報 */
+	
+	// デベロッパＩＤ
+	public static final String APPMART_DEVELOPER_ID = "developer_id";
+	// ライセンスキー
+	public static final String APPMART_LICENSE_KEY = "your_licence_key";
+	// 公開鍵
+	public static final String APPMART_PUBLIC_KEY = "your_public_key";
+	// アプリＩＤ
+	public static final String APPMART_APP_ID = "application_id";
+	// サービスＩＤ
+	public static final String APPMART_SERVICE_ID = "service_id";
+
+	/* appmart課金サービス関連*/
+	
+	// aidlファイルから生成されたサービスクラス
+	private AppmartInBillingInterface service;
+	// 接続状態
+	private boolean isConnected = false;
+	// appmart package
+	public static final String APP_PACKAGE = "jp.app_mart";
+	// サービスパス
+	public static final String APP_PATH = "jp.app_mart.service.AppmartInBillingService";
+	
+	/* その他　*/
+
+	// ＤＥＢＵＧ
+	private boolean isDebug = true;	
+	// アプリコンテキスト
+	private Context mContext;
+	// thread用のhandler
+	private Handler handler = new Handler();
+	// pendingIntent
+	PendingIntent pIntent;
+	// 決済ID
+	private String transactionId;
+	//次回決済ＩＤ
+	private String nextTransactionId;
+	// BroadcastReceiver(決済後）
+	private CheckersReceiver receiver;	
+	
+	public static final String RESULT_CODE 	 	= "resultCode";
+	public static final String PENDING			= "appmart_pending_intent";
+	public static final String BROADCAST		= "appmart_broadcast_return_service_payment";
+	public static final String SERVICE_ID		= "appmart_service_trns_id";
+	public static final String SERVICE_NEXT_ID	= "appmart_service_next_trns_id";
+	
+	
+	/*
+	 * (non-Javadoc)
+	 * @see android.app.Activity#onCreate(android.os.Bundle)
+	 */
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		mContext = getApplicationContext();
+
+		// 決済後のbroadcastをキャッチ
+		setReceiver();
+
+		// appmartサービスに接続するためのIntentオブジェクトを生成
+		Intent i = new Intent();
+		i.setClassName(APP_PACKAGE, APP_PATH);
+		if (mContext.getPackageManager().queryIntentServices(i, 0).isEmpty()) {
+			debugMess("appMartがインストールされていません。");
+			return;
+		}
+
+		// Service Connectionオブジェクト
+		ServiceConnection mConnection = new ServiceConnection() {
+			//接続時実行
+			public void onServiceConnected(ComponentName name,
+					IBinder boundService) {
+				//Ｓｅｒｖｉｃｅクラスをインスタンス化
+				service = AppmartInBillingInterface.Stub.asInterface((IBinder) boundService);
+				isConnected = true;
+				debugMess("appmartサービスの接続が成功しました。");
+			}
+			//切断時実行
+			public void onServiceDisconnected(ComponentName name) {
+				service = null;
+			}
+		};
+
+		// bindServiceを使ってサービスに接続
+		try {
+			bindService(i, mConnection, Context.BIND_AUTO_CREATE);
+		} catch (Exception e) {
+			e.printStackTrace();
+			debugMess("サービス接続できませんでした。");
+		}
+
+		// Handler初期化
+		handler = new Handler() {
+			@SuppressLint("HandlerLeak")
+			@Override
+			public void handleMessage(Message msg) {
+
+				switch (msg.what) {
+				case 1: // pendingIntent取得
+					accessPaymentPage();
+					break;
+				case 2:// パラメータNG
+					debugMess("渡されたパラメータが正しくありません。");
+					break;
+				case 3:// 例外発生
+					debugMess("例外が発生しました。");
+					break;
+				case 10:// 決済最終確認完了					
+					TextView success = (TextView) findViewById(R.id.success_tv);
+					success.setVisibility(View.VISIBLE);
+					
+					break;
+				case -10:// 決済最終確認エラー
+					debugMess("決済確認とれませんでした。");
+					break;
+				}
+
+			}
+
+		};
+
+		// 決済画面を呼ぶボタン
+		Button paymentButton = (Button) findViewById(R.id.access_payment);
+		paymentButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+
+				//接続状態の確認
+				if (isConnected) {
+
+					debugMess("情報取得開始・・・");
+
+					(new Thread(new Runnable() {
+						public void run() {
+							try {
+
+								// 必要なデータを暗号化
+								String dataEncrypted = createEncryptedData(
+										APPMART_SERVICE_ID,
+										APPMART_DEVELOPER_ID,
+										APPMART_LICENSE_KEY, APPMART_PUBLIC_KEY);
+
+								// サービスのprepareForBillingServiceメソッドを呼びます
+								Bundle bundleForPaymentInterface = service.prepareForBillingService(
+												APPMART_APP_ID, dataEncrypted);
+
+								if (bundleForPaymentInterface != null) {
+									
+									int statusId = bundleForPaymentInterface.getInt(RESULT_CODE);
+									if (statusId != 1) {
+										handler.sendEmptyMessage(2);
+										return;
+									} else {
+
+										// PendingIntentを取得
+										pIntent = bundleForPaymentInterface.getParcelable(PENDING);
+										
+										// mainUIに設定
+										handler.sendEmptyMessage(1);
+									}
+
+								}
+
+							} catch (Exception e) {
+								handler.sendEmptyMessage(3);
+								e.printStackTrace();
+							}
+
+						}
+					})).start();
+				}
+			}
+		});
+	}
+
+	/*　BroadcastReceiverの設定 */
+	private void setReceiver() {
+		// Broadcast設定
+		IntentFilter filter = new IntentFilter(BROADCAST);
+		receiver = new CheckersReceiver();
+		registerReceiver(receiver, filter);
+	}
+
+	/* onDestroy */
+	@Override
+	protected void onDestroy() {
+
+		super.onDestroy();
+
+		// appmartサービスからアンバインド
+		unbindService((ServiceConnection) service);
+		service = null;
+
+		// broadcast停止
+		unregisterReceiver(receiver);
+
+	}
+
+	/* 課金画面へリダイレクト */
+	private void accessPaymentPage() {
+
+		try {
+			pIntent.send(mContext, 0, new Intent());
+		} catch (CanceledException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/* debug用 */
+	private void debugMess(String mess) {
+		if (isDebug) {
+			Log.d("DEBUG", mess);
+			Toast.makeText(getApplicationContext(), mess, Toast.LENGTH_SHORT)
+					.show();
+		}
+	}
+
+	/*決済完了後のbroadcastをcatchするReceiverクラス */
+	private class CheckersReceiver extends BroadcastReceiver {
+
+		@Override
+		public void onReceive(Context arg0, Intent arg1) {
+
+			try {
+
+				debugMess("決済が確認されました。");
+
+				// 決済ＩＤを取得
+				transactionId = arg1.getExtras().getString(SERVICE_ID);
+				// 継続決済の場合は次回決済ＩＤを取得
+				nextTransactionId = arg1.getExtras().getString(SERVICE_NEXT_ID);
+
+				// コンテンツを提供し、ＤＢを更新
+				Thread.sleep(1000);
+
+				// 決済を確認
+				(new Thread(new Runnable() {
+					public void run() {
+
+						try {
+
+							int res = service.confirmFinishedTransaction(
+									transactionId, APPMART_SERVICE_ID,
+									APPMART_DEVELOPER_ID);
+
+							if (res == 1) {
+								handler.sendEmptyMessage(10);
+							} else {
+								handler.sendEmptyMessage(-10);
+							}
+
+						} catch (Exception e) {
+							handler.sendEmptyMessage(3);
+							e.printStackTrace();
+						}
+
+					}
+				})).start();
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	/* 引数暗号化 */
+	public String createEncryptedData(String serviceId, String developId,
+			String strLicenseKey, String strPublicKey) {
+
+		final String SEP_SYMBOL = "&";
+		StringBuilder infoDataSB = new StringBuilder();
+		infoDataSB.append(serviceId).append(SEP_SYMBOL);
+
+		// デベロッパID引数を追加
+		infoDataSB.append(developId).append(SEP_SYMBOL);
+
+		// ライセンスキー引数を追加
+		infoDataSB.append(strLicenseKey);
+
+		String strEncryInfoData = "";
+
+		try {
+			KeyFactory keyFac = KeyFactory.getInstance("RSA");
+			KeySpec keySpec = new X509EncodedKeySpec(Base64.decode(
+					strPublicKey.getBytes(), Base64.DEFAULT));
+			Key publicKey = keyFac.generatePublic(keySpec);
+
+			if (publicKey != null) {
+				Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+				cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+
+				byte[] EncryInfoData = cipher.doFinal(infoDataSB.toString()
+						.getBytes());
+				strEncryInfoData = new String(Base64.encode(EncryInfoData,
+						Base64.DEFAULT));
+			}
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			strEncryInfoData = "";
+			debugMess("暗号化は失敗しました。");
+		}
+
+		return strEncryInfoData.replaceAll("(\\r|\\n)", "");
+
+	}
+}
---
+++ src/jp/app_mart/service/AppmartInBillingInterface.aidl
@@ -0,0 +1,25 @@
+package jp.app_mart.service;
+
+import android.os.Bundle;
+
+interface AppmartInBillingInterface {
+
+    //課金前の method
+    Bundle prepareForBillingService(String appId, String encryptedData);
+
+    //サービスの詳細情報を取得
+    String getServiceDetails(String serviceId, String encryptedData);
+
+    //サービス提供後の method
+    int confirmFinishedTransaction(String trnsId, String serviceId, String developerId );
+
+    //トランザクション情報を取得
+    String getPaymentDetails(String trnsId, String serviceId, String developerId);
+
+    //次回支払情報を取得
+    String getNextPaymentDetails(String nextTrnsId, String developerId, String itemId);
+
+    //継続課金の停止
+    String stopContinuePayment(String nextTrnsId, String developerId, String itemId);
+
+}
